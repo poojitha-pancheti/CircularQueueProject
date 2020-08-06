c5b452010155ec3c546e7f91ecf532e1719f2ed9
Poojitha Pancheti <Poojitha.Pancheti@DESKTOP-UK1T43N>
2020-08-06 20:13:25 +0530

    Add project files.

---
+++ CircularQueueProject/Demo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CircularQueueProject
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            int choice, x;
+            CircularQueue qu = new CircularQueue(8);
+            while (true)
+            {
+                Console.WriteLine("1.Insert an element in the queue");
+                Console.WriteLine("2.Delete an element from the queue");
+                Console.WriteLine("3.Display the element at front");
+                Console.WriteLine("4.Display all elements in queue");
+                Console.WriteLine("5.Display the size of queue");
+                Console.WriteLine("6.Quit");
+                Console.WriteLine("Enter your choice:");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 6)
+                    break;
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Entee the element to be inserted:");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        qu.Insert(x);
+                        break;
+                    case 2:
+                        x = qu.Delete();
+                        Console.WriteLine("Element deleted:" + x);
+                        break;
+                    case 3:
+                        Console.WriteLine("Element at the top is:" + qu.Peek());
+                        break;
+                    case 4:
+                        qu.Display();
+                        break;
+                    case 5:
+                        Console.WriteLine("Size of queue:" + qu.Size());
+                        break;
+                    default:
+                        Console.WriteLine("Wrong choice");
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
